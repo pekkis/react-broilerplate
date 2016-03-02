3916f392b7b3cf6239d5d5e15edbaa69b038c5e3
Mikko Forsström <puhemies@diktaattoriporssi.com>
2016-03-02 16:32:44 +0200

    Refactored to react-router-redux major pasko.

--- src/redux.js
+++ src/redux.js
@@ -1,17 +1,16 @@
 import { createStore as reduxCreateStore, applyMiddleware, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { syncHistory, routeReducer } from 'react-router-redux';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux'
 
 export function createStore(
   reducers,
-  history,
+  rawHistory,
   middlewares = [],
   enhancers = []
 ) {
     const createStoreWithMiddleware = compose(
         applyMiddleware(
             thunk,
-            syncHistory(history),
             ...middlewares
         ),
         ...enhancers
@@ -19,9 +18,10 @@ export function createStore(
 
     const reducer = combineReducers({
         ...reducers,
-        routing: routeReducer
+        routing: routerReducer
     });
     const store = createStoreWithMiddleware(reducer);
+    const history = syncHistoryWithStore(rawHistory, store);
 
-    return store;
+    return { history, store };
 }
